168b4ca23d63f57d1ba7663fdc61b4fe36c464de
Rhylen 'Jennifer' Nguyen <rhylennguyen@gmail.com>
2022-06-24 21:24:17 -0400

    Add Update URL

--- ao3_history_export.user.js
+++ ao3_history_export.user.js
@@ -6,6 +6,7 @@
 // @match       https://archiveofourown.org/users/*/readings
 // @grant       none
 // @require     https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js
+// @updateURL   https://github.com/riceconfetti/ao3_history_export/raw/main/ao3_history_export.user.js
 // @require     https://github.com/riceconfetti/ao3_history_export/raw/main/ao3_history_export.user.js
 // ==/UserScript==
 
