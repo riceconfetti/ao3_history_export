514d2d3875fa5f4cc1d80971289ce67939bf3ed3
Rhylen 'Jennifer' Nguyen <rhylennguyen@gmail.com>
2022-06-24 21:47:14 -0400

    Remove require

--- ao3_history_export.user.js
+++ ao3_history_export.user.js
@@ -7,7 +7,6 @@
 // @grant       none
 // @require     https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js
 // @updateURL   https://github.com/riceconfetti/ao3_history_export/raw/main/ao3_history_export.user.js
-// @require     https://github.com/riceconfetti/ao3_history_export/raw/main/ao3_history_export.user.js
 // ==/UserScript==
 
 $('.navigation.actions').append('<li><a id="ao3_download_history">Download</span></li>');
