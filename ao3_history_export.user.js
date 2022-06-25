cf82b982d4c316c171a578c0c6c1ec4a4a1487a0
Rhylen 'Jennifer' Nguyen <rhylennguyen@gmail.com>
2022-06-24 21:14:17 -0400

    Added Download Button

--- ao3_history_export.user.js
+++ ao3_history_export.user.js
@@ -1,7 +1,17 @@
 // ==UserScript==
-// @name     AO3 History Export
-// @version  1
-// @grant    none
-// @require  https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js
-// @require  https://
+// @name        AO3 History Export
+// @namespace   https://github.com/riceconfetti
+// @version     1
+// @description Export reading history to csv.
+// @match       https://archiveofourown.org/users/*/readings
+// @grant       none
+// @require     https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js
+// @require     https://github.com/riceconfetti/ao3_history_export/raw/main/ao3_history_export.user.js
 // ==/UserScript==
+
+$('.navigation.actions').append("<li><a id="ao3_download_history">Download</span></li>");
+$('#a03_download_history').click(downloadHistory);
+
+function downloadHistory() {
+
+}
