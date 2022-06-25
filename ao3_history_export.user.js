f0ed41ab6610d639b05eb765988972ec1f55dbe2
Rhylen 'Jennifer' Nguyen <rhylennguyen@gmail.com>
2022-06-24 20:47:20 -0400

    Create ao3_history_export.user.js

---
+++ ao3_history_export.user.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name     AO3 History Export
+// @version  1
+// @grant    none
+// @require  https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js
+// @require  https://
+// ==/UserScript==
